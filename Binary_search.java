83936f5221dd8516a6867a1b7fc92481e7eb03e8
Sanketgh007 <136904423+Sanketgh007@users.noreply.github.com>
2024-07-10 16:42:27 +0530

    Update Binary_search.java

--- Binary_search.java
+++ Binary_search.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class Binary_search {
 	
 	public static void main(String[] args) {
-		int ar[]=new int[100];
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter how many element present in sorted array");
 		int n=sc.nextInt();	
+		int ar[]=new int[n];
 		System.out.println("Enter array element in sorted order");
 		for(int i=0;i<n;i++)
 		{
@@ -20,28 +20,46 @@ public class Binary_search {
 		int key=sc.nextInt();
          int low=0,high=n-1;
     	 int mid;
-         
+         boolean isAsc=ar[low]<ar[high];
+         boolean found=false;
          while(low<=high)
 		 {
 			 mid=(low+high)/2;
+			 
 			 if(ar[mid]==key)
 			 {
 				 System.out.println("Key " +key+" found at position "+mid);
+				 found=true;
 				 break;
 			 }
-			 else if(key>ar[mid])
+			 if(isAsc) {
+			   if(key>ar[mid])
 			 {
 				 low=mid+1;
 			 }
-			 else if(key<ar[mid])
+			 else if (key<ar[mid])
 			 {
 				 high=mid-1;
-			 }
+			 }}
 			 else
-			 {
-				System.out.println("Element not found");
+			 {    
+				 if(key>ar[mid])
+				 {
+					 high=mid-1;
+				 }
+				 else if (key<ar[mid])
+				 {
+					 low=mid+1;
+				 }
 			 }
+			
+			 }
+         if (!found)
+         {
+        	 System.out.println("Searched element not present in the array");
+         }
+         
 		 }
 	}
 	
-}
+
